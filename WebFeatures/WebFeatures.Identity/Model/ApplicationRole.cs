60e58d79c7baa1e8b75ca060b53ea2b2e3f98ac2
Vyacheslav Chuletc <chuletsva@gmail.com>
2020-03-10 20:00:39 +0200

    removed old files

--- WebFeatures/WebFeatures.Identity/Model/ApplicationRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace WebFeatures.Identity.Model
-{
-    public class ApplicationRole : IdentityRole
-    {
-        public string Description { get; set; }
-    }
-}
--- WebFeatures/WebFeatures.Identity/Model/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace WebFeatures.Identity.Model
-{
-    public class ApplicationUser : IdentityUser
-    {
-    }
-}
