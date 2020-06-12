ae2e8e909c171787217c7c19b0accfcd584fc018
Vyacheslav Chuletc <chuletsva@gmail.com>
2020-06-12 13:01:13 +0200

    average rating for product

--- WebFeatures/Scripts/Schema/Functions.sql
+++ WebFeatures/Scripts/Schema/Functions.sql
@@ -4,13 +4,14 @@ $functions$ BEGIN
 CREATE FUNCTION get_product(product_id UUID)
 RETURNS TABLE
 (
-	Id UUID,
-	Name VARCHAR,
-	Price DECIMAL,
-	Description VARCHAR,
-	ManufacturerId UUID,
-	CategoryId UUID,
-	BrandId UUID
+	id UUID,
+	name VARCHAR,
+	price DECIMAL,
+	description VARCHAR,
+	manufacturerid UUID,
+	categoryid UUID,
+	brandid UUID,
+	averagerating INT
 ) AS $$
 BEGIN
 	RETURN QUERY 
@@ -21,11 +22,22 @@ BEGIN
 		p.description,
 		p.manufacturerid,
 		p.categoryid,
-		p.brandid
+		p.brandid,
+		AVG(r.rating)::INT
 	FROM
 		public.products p
-	WHERE 
-		p.id = product_id;
+	LEFT JOIN
+		public.productreviews r ON r.productid = p.id
+	WHERE
+		p.id = product_id
+	GROUP BY
+		p.id,
+		p.name,
+		p.price,
+		p.description,
+		p.manufacturerid,
+		p.categoryid,
+		p.brandid;
 
 END; $$ LANGUAGE 'plpgsql';
 
